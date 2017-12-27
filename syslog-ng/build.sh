5319e295acb364df26c1c47448f39fd78e57241b
Chinaxiang <huangyanxiang2012@gmail.com>
2017-12-27 18:14:15 +0800

    xx

--- syslog-ng/build.sh
+++ syslog-ng/build.sh
@@ -6,8 +6,8 @@ export SYSLOG_VERSION=3.13.2
 export DOWNLOAD_URL="https://github.com/balabit/syslog-ng/releases/download/syslog-ng-${SYSLOG_VERSION}/syslog-ng-${SYSLOG_VERSION}.tar.gz"
 
 apk update
-apk add gcc flex bison libpcre glib pcre autoconf
-apk add curl alpine-sdk
+apk add gcc flex bison glib pcre autoconf
+apk add curl alpine-sdk pcre-dev
 
 cd /tmp
 curl -L "${DOWNLOAD_URL}" > "syslog-ng-${SYSLOG_VERSION}.tar.gz"
@@ -19,5 +19,5 @@ make install
 cd ..
 rm -rf "syslog-ng-${SYSLOG_VERSION}" "syslog-ng-${SYSLOG_VERSION}.tar.gz"
 
-apk del curl alpine-sdk gcc flex bison libpcre glib pcre autoconf
+apk del curl alpine-sdk gcc flex bison glib pcre autoconf
 rm -rf /var/cache/apk/*
