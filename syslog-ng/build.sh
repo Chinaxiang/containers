6e66d718faaf660ccf47441ce2076f779e290d2e
Chinaxiang <huangyanxiang2012@gmail.com>
2017-12-27 17:47:05 +0800

    edit

--- syslog-ng/build.sh
+++ syslog-ng/build.sh
@@ -2,18 +2,12 @@
 
 set -e
 
-echo "http://mirror.yandex.ru/mirrors/alpine/v3.2/main/"       > /etc/apk/repositories
-echo "@3.1 http://mirror.yandex.ru/mirrors/alpine/v3.1/main/" >> /etc/apk/repositories
-
-export SYSLOG_VERSION=3.6.3
-
+export SYSLOG_VERSION=3.13.2
 export DOWNLOAD_URL="https://github.com/balabit/syslog-ng/releases/download/syslog-ng-${SYSLOG_VERSION}/syslog-ng-${SYSLOG_VERSION}.tar.gz"
 
 apk update
-
-apk add glib pcre libeventlog@3.1
-
-apk add curl alpine-sdk glib-dev pcre-dev libeventlog-dev@3.1
+apk add gcc flex bison libpcre glib pcre autoconf
+apk add curl alpine-sdk
 
 cd /tmp
 curl -L "${DOWNLOAD_URL}" > "syslog-ng-${SYSLOG_VERSION}.tar.gz"
@@ -25,4 +19,5 @@ make install
 cd ..
 rm -rf "syslog-ng-${SYSLOG_VERSION}" "syslog-ng-${SYSLOG_VERSION}.tar.gz"
 
-apk del curl alpine-sdk glib-dev pcre-dev libeventlog-dev
+apk del curl alpine-sdk gcc flex bison libpcre glib pcre autoconf
+rm -rf /var/cache/apk/*
